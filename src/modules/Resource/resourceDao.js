a87e910d126fabe96a8f0dadd75deca6dfcf3cef
Lynn-Apedo <lynnapedo@gmail.com>
2021-04-20 18:48:30 +0200

    add Borrow table, get and post borrow

--- src/modules/Resource/resourceDao.js
+++ src/modules/Resource/resourceDao.js
@@ -5,6 +5,7 @@ class ResourceDao extends Model {
   static init(sequelize) {
     return super.init(
       {
+        borrowId: DataTypes.INTEGER,
         title: DataTypes.STRING,
         page_number: DataTypes.INTEGER,
         publication_date: DataTypes.DATE,
@@ -16,6 +17,14 @@ class ResourceDao extends Model {
   }
   static associate(models) {
     //define association here
+
+    this.belongsTo(
+      models.BorrowDao,{
+        foreignKey: {
+          name:"borrowId",
+      }
+      }
+    );
     return this;
   }
   
--- src/modules/Resource/resourceEntity.js
+++ src/modules/Resource/resourceEntity.js
@@ -1,6 +1,7 @@
 class ResourceEntity {
-    constructor({id, title, page_number, publication_date, description, quantity }) {
+    constructor({id, borrowId, title, page_number, publication_date, description, quantity }) {
         this.id = id;
+        this.borrowId = borrowId;
         this.title = title;
         this.page_number = page_number;
         this.publication_date = publication_date;
